85ad82a9282e4b8f723ca8851c91c6529affacd7
Joshwin Greene <joshwin.greene@gmail.com>
2021-01-04 08:17:48 -0800

    Added Core functionality and able to test via console

--- Palindrome.Client/Program.cs
+++ Palindrome.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Palindrome.Domain.Models;
 
 namespace Palindrome.Client
 {
@@ -6,7 +7,17 @@ namespace Palindrome.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter text to check");
+            string givenStr = Console.ReadLine();
+
+            PalindromeChecker checker = new PalindromeChecker(givenStr);
+
+            if (checker.CheckIfPalindrome())
+            {
+                System.Console.WriteLine(checker.ToString() +  " is a palindrome");
+            } else {
+                System.Console.WriteLine(checker.ToString() +  " not a palindrome");
+            }
         }
     }
 }
